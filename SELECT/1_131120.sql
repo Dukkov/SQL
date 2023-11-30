0db7a8dd220585cc7dce10660f6876c37c0d73d5
Dukov <rhtkdals135@gmail.com>
2023-11-30 16:01:40 +0900

    select 7th problem solved

---
+++ SELECT/1_131120.sql
@@ -0,0 +1,5 @@
+-- 3월에 태어난 여성 회원 목록 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131120
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, "%Y-%m-%d") AS DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE MONTH(DATE_OF_BIRTH) = 3 AND TLNO IS NOT NULL AND GENDER="W";
