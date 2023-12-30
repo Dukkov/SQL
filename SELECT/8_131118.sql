e5b2f64497ea101d5c87e3ac922a69dcb1f90748
rhtkdals135 <rhtkdals135@gmail.com>
2023-12-30 23:44:09 +0900

    문제번호 수정

--- SELECT/8_131118.sql
+++
@@ -1,5 +0,0 @@
--- 서울에 위치한 식당 목록 출력하기
--- https://school.programmers.co.kr/learn/courses/30/lessons/131118
-SELECT
-FROM
-WHERE
--- SELECT/9_131118.sql
+++ SELECT/9_131118.sql
@@ -1,2 +1,2 @@
-서울에 위치한 식당 목록 출력하기
-https://school.programmers.co.kr/learn/courses/30/lessons/131118
+-- 서울에 위치한 식당 목록 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131118
