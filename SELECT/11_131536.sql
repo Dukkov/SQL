8f5779b2f44ac0af158c491dd54ee7e3a5345315
rhtkdals135 <rhtkdals135@gmail.com>
2024-01-26 23:51:23 +0900

    select 11th problem solved

--- SELECT/11_131536.sql
+++ SELECT/11_131536.sql
@@ -1,7 +1,10 @@
 -- 재구매가 일어난 상품과 회원 리스트 구하기
 -- https://school.programmers.co.kr/learn/courses/30/lessons/131536
--- SELECT
--- FROM
--- WHERE
--- ORDER BY
--- LIMIT
+SELECT USER_ID, PRODUCT_ID
+FROM (
+    SELECT USER_ID, PRODUCT_ID, COUNT(*) AS purchase_cnt
+    FROM ONLINE_SALE
+    GROUP BY USER_ID, PRODUCT_ID
+) AS subquery
+WHERE purchase_cnt > 1
+ORDER BY USER_ID ASC, PRODUCT_ID DESC;
