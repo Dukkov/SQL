aa7bfb9dc67ffe4ad6c41530e4d5a5f7be5ca59e
rhtkdals135 <rhtkdals135@gmail.com>
2024-01-29 23:13:45 +0900

    select 13th problem start

--- SELECT/11_131536.sql
+++ SELECT/11_131536.sql
@@ -2,9 +2,9 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/131536
 SELECT USER_ID, PRODUCT_ID
 FROM (
-    SELECT USER_ID, PRODUCT_ID, COUNT(*) AS purchase_cnt
-    FROM ONLINE_SALE
-    GROUP BY USER_ID, PRODUCT_ID
+  SELECT USER_ID, PRODUCT_ID, COUNT(*) AS purchase_cnt
+  FROM ONLINE_SALE
+  GROUP BY USER_ID, PRODUCT_ID
 ) AS subquery
 WHERE purchase_cnt > 1
 ORDER BY USER_ID ASC, PRODUCT_ID DESC;
